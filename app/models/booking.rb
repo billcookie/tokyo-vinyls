91d065186da6536939cd131b7402d90fa9359303
Josh Chappelow <jdchappelow@gmail.com>
2022-08-15 17:30:46 +0900

    added basic validations and relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :vinyl
   belongs_to :user
+
+  has_many :bookings
 end
