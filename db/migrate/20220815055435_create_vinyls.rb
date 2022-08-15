f136556448ced1a469690e8569fa6f53ac9a64c8
Carla Willmes <willmes.carla@gmail.com>
2022-08-15 15:02:45 +0900

    created vinyl model

---
+++ db/migrate/20220815055435_create_vinyls.rb
@@ -0,0 +1,13 @@
+class CreateVinyls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vinyls do |t|
+      t.string :name
+      t.string :genre
+      t.string :song
+      t.string :artist
+      t.integer :publishing_year
+
+      t.timestamps
+    end
+  end
+end
