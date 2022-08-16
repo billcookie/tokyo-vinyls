e3a19bce940914f7ceb06e70b9250103dfd28611
Bill Cook <billcook8122@gmail.com>
2022-08-16 15:21:04 +0900

    added bookings controller and create function with pundit.

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,8 +1,25 @@
 class BookingPolicy < ApplicationPolicy
+
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+      # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
   end
 end
