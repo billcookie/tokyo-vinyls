c23e4931d15f3a855f6fd29d9e52f1fb196d5c1e
Bill Cook <billcook8122@gmail.com>
2022-08-16 17:46:06 +0900

    finally finished the basic create and index function of pundit

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -3,7 +3,7 @@ class BookingPolicy < ApplicationPolicy
   class Scope < Scope
       # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.where(user: user)
     end
   end
 
