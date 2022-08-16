06447bc8f5fc4f47daef3a07eddaf370b2028193
Josh Chappelow <jdchappelow@gmail.com>
2022-08-16 18:37:00 +0900

    routes and nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:index]
   resources :vinyls, only: [:index, :show]
   resources :offers, only: [:new, :show, :index, :create] do
-    resources :bookings, only: [:index, :create]
+    resources :bookings, only: [:create]
   end
 
   # Defines the root path route ("/")
