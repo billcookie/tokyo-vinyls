03ef6a67bb1cbbf4b7c90f80283d1ee177c7c605
Bill Cook <billcook8122@gmail.com>
2022-08-16 12:43:51 +0900

    seeder works now with images and vinyl index page displays images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_091638) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_030333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_091638) do
     t.integer "publishing_year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url"
   end
 
   add_foreign_key "bookings", "offers"
