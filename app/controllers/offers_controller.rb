cddc147c6c044165bc07900c1449fe15192cd58b
Josh Chappelow <jdchappelow@gmail.com>
2022-08-16 16:02:38 +0900

    messy file problems

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,15 +1,4 @@
 class OffersController < ApplicationController
 
-  def show
-    @offer = Offer.find(params[:id])
-    @offer = Offer.new
-    @offers = Offer.all
-  end
-
-  def index
-    @booking = Booking.new
-    @offer = Offer.new
-    @offers = Offer.all
-  end
 
 end
