62d2242a54e29ef92f1215ceacf1f0053d01edc2
Bill Cook <billcook8122@gmail.com>
2022-08-17 18:20:28 +0900

    checking geocode

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -3,6 +3,12 @@ class VinylsController < ApplicationController
   skip_after_action :verify_authorized
   def index
     @vinyls = policy_scope(Vinyl)
+    @markers = @offers.geocoded.map do |offer|
+      {
+        lat: offer.latitude,
+        lng: offer.longitude
+      }
+    end
   end
 
   def show
