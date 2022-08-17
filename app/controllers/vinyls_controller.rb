98f1dbd575c6dc5ca67558980a2763993acb24dd
Bill Cook <billcook8122@gmail.com>
2022-08-17 18:27:11 +0900

    corrected errors

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -3,12 +3,6 @@ class VinylsController < ApplicationController
   skip_after_action :verify_authorized
   def index
     @vinyls = policy_scope(Vinyl)
-    @markers = @offers.geocoded.map do |offer|
-      {
-        lat: offer.latitude,
-        lng: offer.longitude
-      }
-    end
   end
 
   def show
