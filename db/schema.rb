9adcf6991bfeb77e337a436271921e4d7d81183f
Bill Cook <billcook8122@gmail.com>
2022-08-17 19:19:52 +0900

    t.connect Refference error mapboxgl is not defined

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_030333) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_101313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_030333) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_offers_on_user_id"
     t.index ["vinyl_id"], name: "index_offers_on_vinyl_id"
   end
