b06e8992c7fd010418f2ba4bed1b5bfc55898dd3
Bill Cook <billcook8122@gmail.com>
2022-08-18 10:51:07 +0900

    artist model completed and bundle file also updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_025857) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2022_08_17_101313) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
