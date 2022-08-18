a105d2c9b4963e703e05dbacd363f99a15a3b0c6
Josh Chappelow <jdchappelow@gmail.com>
2022-08-18 13:25:08 +0900

    search function added to home

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ApplicationRecord
   has_many :vinyls
+  include PgSearch::Model
 end
--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -3,4 +3,14 @@ class Vinyl < ApplicationRecord
   has_many :offers
   validates :name, presence: true
   validates :artist, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :song_artist_search,
+    against: [:name, :song],
+    associated_against: {
+      artist: [:name]
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
